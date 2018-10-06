66e9a9a049040598c838f46ba62bc216cab36d95
Casey Duckering <cduckering@gmail.com>
2018-10-06 14:09:35 -0500

    Add log depth and construction

--- cirq/qutrit/plus_k_gate.py
+++ cirq/qutrit/plus_k_gate.py
@@ -40,12 +40,13 @@ class PlusKGate(raw_types.TernaryLogicGate,
         return val_to_bits(reg_val, len(trits))
 
     def default_decompose(self, qubits):
-        yield from self._gen_partial_carry(qubits, self.k, top=True)
+        yield from self._gen_all_carry(qubits, self.k, top=True)
 
         if self.k[0]:
             yield common_gates.F01(qubits[0])
 
-    def _gen_partial_carry(self, qubits, k, top=True):
+    @classmethod
+    def _gen_all_carry(cls, qubits, k, top=True):
         if len(qubits) <= 0: return
         elif len(qubits) == 1:
             return#yield common_gates.PlusOne(qubits[0])
@@ -59,8 +60,8 @@ class PlusKGate(raw_types.TernaryLogicGate,
         else:
             half = len(qubits) // 2
             yield PlusKCarryGate(k[:half], not top)(*qubits[:half+1])
-            yield from self._gen_partial_carry(qubits[:half], k[:half], top)
-            yield from self._gen_partial_carry(qubits[half:], k[half:], False)
+            yield from cls._gen_all_carry(qubits[:half], k[:half], top)
+            yield from cls._gen_all_carry(qubits[half:], k[half:], False)
             yield PlusKUncarryAddGate(k[:half], not top)(*qubits[:half+1])
             if k[half]:
                 yield common_gates.F01(qubits[half])
@@ -104,9 +105,11 @@ class PlusKCarryGate(raw_types.TernaryLogicGate,
         return trits
 
     def default_decompose(self, qubits):
-        return (
-            # TODO
-        )
+        if self._inverted:
+            op = common_gates.MinusOne(qubits[-1])
+        else:
+            op = common_gates.PlusOne(qubits[-1])
+        return _gen_or_and_control_u(op, qubits[:-1], k, self.carry_in)
 
 
 class PlusKUncarryAddGate(raw_types.TernaryLogicGate,
@@ -147,6 +150,90 @@ class PlusKUncarryAddGate(raw_types.TernaryLogicGate,
         return trits
 
     def default_decompose(self, qubits):
-        return (
-            # TODO
-        )
+        op = common_gates.F02(qubits[-1])
+        not_ops = [common_gates.F01(q) for q in qubits[1:-1]]
+        yield from not_ops
+        yield from _gen_or_and_control_u(op, qubits[:-1], k, self.carry_in)
+        yield from not_ops
+
+
+'''class LogAndUpwardGate(raw_types.TernaryLogicGate,
+                       ops.ReversibleEffect,
+                       ops.CompositeGate,
+                       ops.TextDiagrammable):
+    def inverse(self):
+        ...
+
+    def text_diagram_info(self, args):
+        syms = ['[And ]'] * args.known_qubit_count
+        syms[0] = '[And>]'
+        return ops.TextDiagramInfo(tuple(syms))
+
+    def validate_trits(self, trits):
+        super().validate_trits(trits)
+        assert len(trits) == len(self.k)+1, (
+               'Gate only operates on len(k)+1 qutrits')
+
+    def applied_to_trits(self, trits):
+        k_val = bits_to_val(self.k)
+        if self.carry_in:
+            reg_val = bits_to_val(trits[1:-1]) << 1
+            k_val |= 1
+            reg_val |= trits[0] == 2 or (trits[0] == 1 and self.k[0])
+        else:
+            reg_val = bits_to_val(trits[:-1])
+        reg_val ^= ~(~0 << (len(trits)-2)) << 1  # Invert controls except first
+        if (k_val + reg_val).bit_length() > len(self.k):
+            trits[-1] = (trits[-1] * 2 - 1) % 3
+        return trits
+
+    def default_decompose(self, qubits):
+        op = common_gates.F02(qubits[-1])
+        not_ops = [common_gates.F01(q) for q in qubits[1:-1]]
+        yield from not_ops
+        yield from _gen_or_and_control_u(op, qubits[:-1], k, self.carry_in)
+        yield from not_ops'''
+
+
+def _gen_or_and_control_u(controlled_op, qubits, k, carry_in):
+    ...
+
+
+def _gen_log_and_upward(qubits, qubit_mask=None):
+    if qubit_mask is None:
+        qubit_mask = {q: True for q in qubits}
+    n = len(qubits)
+    if n == 0: return
+    elif n == 1: return
+    elif n == 2:
+        assert qubit_mask[qubits[0]]
+        qubit_mask[qubits[0]] = False
+        yield common_gates.C1PlusOne(qubits[1], qubits[0])
+    elif _is_pow_2(n+1):
+        yield from _gen_log_and_upward(qubits[1:1+n//2], qubit_mask)
+        yield from _gen_log_and_upward(qubits[1+n//2:], qubit_mask)
+        assert qubit_mask[qubits[0]]
+        qubit_mask[qubits[0]] = False
+        yield common_gates.ControlledTernaryGate(common_gates.PlusOne,
+                                ((1 if qubit_mask[qubits[1]] else 2,),
+                                 (1 if qubit_mask[qubits[1+n//2]] else 2,)))(
+                            qubits[1], qubits[1+n//2], qubits[0])
+    else:
+        nice_n = (1 << ((n+1).bit_length() - 1)) - 1
+        yield from _gen_log_and_upward(qubits[1:-nice_n], qubit_mask)
+        yield from _gen_log_and_upward(qubits[-nice_n:], qubit_mask)
+        assert qubit_mask[qubits[0]]
+        qubit_mask[qubits[0]] = False
+        if nice_n + 1 == n:
+            yield common_gates.ControlledTernaryGate(common_gates.PlusOne,
+                                ((1 if qubit_mask[qubits[-nice_n]] else 2,),))(
+                            qubits[-nice_n], qubits[0])
+        else:
+            yield common_gates.ControlledTernaryGate(common_gates.PlusOne,
+                                ((1 if qubit_mask[qubits[1]] else 2,),
+                                 (1 if qubit_mask[qubits[-nice_n]] else 2,)))(
+                            qubits[1], qubits[-nice_n], qubits[0])
+
+
+def _is_pow_2(n):
+    return n == 1 << (n.bit_length() - 1)
